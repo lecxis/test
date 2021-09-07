6009dea7ace7ee1e0f7204966cef1a2abaef8741
Olalekan Olonilua <oloniluaolalekan@gmail.com>
2021-09-07 09:43:07 +0100

    mobile view of a phone page

--- spec/testSpec.js
+++ spec/testSpec.js
@@ -1,7 +1,39 @@
-var passwordIsValid=require('../src/index.js');
+var {passwordIsValid,passwordStrength}=require('../src/index.js');
 
 describe("Password is valid", () => {
     it("password exist", () => {
-      expect(passwordIsValid('mypass')).toBe(true);
+     // passwordIsValid('');
+      expect(function(){passwordIsValid('')}).toThrow(new Error('password should exist'));
+    });
+    it("password should be longer than 8 characters", () => {
+      // passwordIsValid('');
+       expect(function(){passwordIsValid('abcdefgh')}).toThrow(new Error('password should be longer than than 8 characters'));
+     });
+     it("password should contain at least one lower case characters", () => {
+      // passwordIsValid('');
+       expect(function(){passwordIsValid('ABSGVTHSA')}).toThrow(new Error('password should have at least one lowercase letter'));
+     });
+    it("password should contain at least one upper case characters", () => {
+      // passwordIsValid('');
+       expect(function(){passwordIsValid('a1234b56789')}).toThrow(new Error('password should have at least one uppercase letter'));
+     });
+     it("password should contain at least one digit", () => {
+      // passwordIsValid('');
+       expect(function(){passwordIsValid('abghdbABaaa')}).toThrow(new Error('password should at least have one digit'));
+     });
+     it("password should contain at least one special character", () => {
+      // passwordIsValid('');
+       expect(function(){passwordIsValid('abghdbABaaa78')}).toThrow(new Error('password should have at least one special character'));
+     });
+     it("password should contain at least one whitespace character", () => {
+      // passwordIsValid('');
+       expect(function(){passwordIsValid('abghdbABa*aa78')}).toThrow(new Error('password should have at least one whitespace character'));
+     });
+});
+
+describe("Password strength", () => {
+    it("password is strong", () => {
+     // passwordIsValid('');
+      expect(passwordStrength('asdefg hh8FGi')).toBe('strong');
     });
 });
--- src/index.js
+++ src/index.js
@@ -1,12 +1,112 @@
-
+let count=0;
 function passwordIsValid(password){
+    let arr=password.split('');
 if (!password){
-    return false
+    throw new Error('password should exist');
 }
+else{
+    count++;
+}
+
+if (arr.length<=8){
+    throw new Error('password should be longer than than 8 characters');
+}
+else{
+    count++;
+}
+if (!testLowerCase(arr)){
+    throw new Error('password should have at least one lowercase letter');
+}
+else{
+    count++;
+}
+if (!testUpperCase(arr)){
+    throw new Error('password should have at least one uppercase letter');
+}
+else{
+    count++;
+}
+if (!testDigit(arr)){
+    throw new Error('password should at least have one digit');
+}
+else{
+    count++;
+}
+if (!testSpecial(arr)){
+    throw new Error('password should have at least one special character');
+}
+else{
+    count++;
+}
+if (!testWhitespace(arr)){
+    throw new Error('password should have at least one whitespace character');
+}
+else{
+    count++;
+}
+    
 return true;
 }
 function passwordStrength(password){
-
+count=0;
+strength='';
+  try{
+passwordIsValid(password);
+}
+ catch(err){
+         //console.log(err);
+    }
+console.log('password strength determined');
+console.log(count);
+if (count>=6){
+    strength='strong';
+}
+return strength;
 }
 
-module.exports= passwordIsValid;
+function testLowerCase(arry){
+    let state=false
+arry.forEach(element => {
+    if (/[a-z]/.test(element)){
+        state=true; 
+    }
+});
+return state;
+}
+function testUpperCase(array){
+    let state=false;
+    array.forEach(element => {
+        if (/[A-Z]/.test(element)){
+            state= true;
+        }
+    });
+    return state;
+    }
+    function testDigit(array){
+    let state=false;
+    array.forEach(element => {
+        if (/[0-9]/.test(element)){
+            state= true;
+        }
+    });
+    return state;
+    }
+    function testSpecial(array){
+    let state=false;
+    array.forEach(element => {
+        if (/[^a-zA-Z0-9]/.test(element)){
+            state= true;
+        }
+    });
+    return state;
+    }
+    function testWhitespace(array){
+    let state=false;
+    array.forEach(element => {
+        if (/[\s]/.test(element)){
+            state= true;
+        }
+    });
+    return state;
+    }
+module.exports= {passwordIsValid,passwordStrength};
